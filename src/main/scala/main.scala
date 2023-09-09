f10836047713450757b22225b1d9a52434eeff40
Consultant <samirsamado5@gmail.com>
2023-09-09 09:58:36 -0400

    forLoop with conditions

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,9 +1,24 @@
 @main
 def main(): Unit = {
-  var mylist = List(57,234,23,4,23423,4)
-  for (item <- mylist) {
-    println(item)
-    println(", ")
+  def print_on_specific_condition(): Unit = {
+    for
+      i <- 1 to 5
+      j <- 'a' to 'e'
+      if i == 2
+      if j == 'b'
+    do println(s"i = $i and j = $j")
   }
+
+  def forloopFruits(): Unit = {
+    val fruits = List("Banana", "Apple", "Lime", "Orange")
+
+    val fruitLength = for
+      f <- fruits
+      if f.length > 4
+    yield
+      f.length
+  }
+
+  print(forloopFruits())
 }
 
