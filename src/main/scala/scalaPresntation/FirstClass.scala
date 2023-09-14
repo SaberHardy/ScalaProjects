c949c03d170d3ee617d9655ae14347a693a2fd6e
Consultant <samirsamado5@gmail.com>
2023-09-14 19:20:40 -0400

    pure Function

--- src/main/scala/scalaPresntation/FirstClass.scala
+++ src/main/scala/scalaPresntation/FirstClass.scala
@@ -2,11 +2,15 @@ package scalaPresntation
 
 @main
 def main(): Unit = {
-  val add = (a: Int, b: Int) => a + b
-  def operate(a: Int, b: Int, op: (Int, Int) => Int): Int = op(a, b)
-  val result = operate(3, 5, add) // Result will be 8
+  //  val add = (a: Int, b: Int) => a + b
+  //  def operate(a: Int, b: Int, op: (Int, Int) => Int): Int = op(a, b)
+  //  val result = operate(3, 5, add) // Result will be 8
 
-//  println(result)
+  //  println(result)
 
+  // Pure Functions
+  def pureFunction(x: Int, y: Int): Int = x + y
+
+  println(pureFunction(2, 3))
 }
 
