eb3c65b9cce8916cf99c9a05a6f52e4d82ab8636
Consultant <samirsamado5@gmail.com>
2023-09-14 19:29:25 -0400

    Immutable Function

--- src/main/scala/scalaPresntation/FirstClass.scala
+++ src/main/scala/scalaPresntation/FirstClass.scala
@@ -5,12 +5,18 @@ def main(): Unit = {
   //  val add = (a: Int, b: Int) => a + b
   //  def operate(a: Int, b: Int, op: (Int, Int) => Int): Int = op(a, b)
   //  val result = operate(3, 5, add) // Result will be 8
-
   //  println(result)
 
   // Pure Functions
-  def pureFunction(x: Int, y: Int): Int = x + y
+  //  def pureFunction(x: Int, y: Int): Int = x + y
+  //  println(pureFunction(2, 3))
 
-  println(pureFunction(2, 3))
+  // Immutable Collections
+  //  val myList = List(1, 2, 3, 4)
+  //  val doubledList = myList.map(x => x * 2) // Creates a new list with doubled values
+  //  println(doubledList)
+
+
+
 }
 
