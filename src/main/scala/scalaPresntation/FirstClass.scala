da21f8e67b58424a9f4b7951ff32904867f0a118
Consultant <samirsamado5@gmail.com>
2023-09-15 11:43:42 -0400

    factorial number

--- src/main/scala/scalaPresntation/FirstClass.scala
+++ src/main/scala/scalaPresntation/FirstClass.scala
@@ -5,33 +5,34 @@ import scala.util.{Failure, Success}
 @main
 def main(): Unit = {
   /* First-Class Functions */
-  //  val add = (a: Int, b: Int) => a + b
-  //  def operate(a: Int, b: Int, op: (Int, Int) => Int): Int = op(a, b)
-  //  val result = operate(3, 5, add) // Result will be 8
-  //  println(result)
+  //    val add = (a: Int, b: Int) => a + b
+  //    def operate(a: Int, b: Int, op: (Int, Int) => Int): Int = op(a, b)
+  //    val result = operate(3, 5, add) // Result will be 8
+  //    println(result)
 
   /* Pure Functions */
-  //  def pureFunction(x: Int, y: Int): Int = x + y
-  //  println(pureFunction(2, 3))
+  //    def pureFunction(x: Int, y: Int): Int = x + y
+  //    println(pureFunction(2, 3))
 
   /* Immutable Collections */
-  //  val myList = List(1, 2, 3, 4)
-  //  val doubledList = myList.map(x => x * 2) // Creates a new list with doubled values
-  //  println(doubledList)
+//    val myList = List(1, 2, 3, 4)
+//    val doubledList = myList.map(x => x * 2) // Creates a new list with doubled values
+//    println(doubledList)
 
   /*  Pattern Matching​ */
-  //  def matchExample(x: Int): String = x match {
-  //    case 0 => "Zero"
-  //    case 1 => "One"
-  //    case _ => "Other"
-  //  }
-  //  println(matchExample(3))
+//    def matchExample(x: Int): String = x match {
+//      case 0 => "Zero"
+//      case 1 => "One"
+//      case _ => "Other"
+//    }
+//    println(matchExample(1))
 
   /* Recursion */
-  //  def factorial(n: Int): Int = {
-  //    if (n <= 1) 1
-  //    else n * factorial(n - 1)
-  //  }
+//    def factorial(n: Int): Int = {
+//      if (n <= 1) 1
+//      else n * factorial(n - 1)
+//    }
+//  println(factorial(5))
 
   /*Implicit Functions*/
   // Define a class
