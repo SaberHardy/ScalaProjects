9949e1aff8defd03ef8709f0e606eeb88fb1505f
Consultant <samirsamado5@gmail.com>
2023-09-17 18:31:42 -0400

    pass function as input to another function

--- src/main/scala/tasteScala/FunctionalProgramming.scala
+++ src/main/scala/tasteScala/FunctionalProgramming.scala
@@ -10,25 +10,32 @@ def FunctionalProgramming(): Unit = {
 
   // 1 Lambdas functions
   //1.a = Built-in functions
-  val a = List(2, 4, 6, 5, 4, 3).map(i => i % 2)
+  //  val a = List(2, 4, 6, 5, 4, 3).map(i => i % 2)
+  //
+  //  //  1.a = User function
+  //
+  //  val l1 = List(1, 1, 2, 3, 5, 8)
+  //  val l2 = List(13, 21, 34)
+  //
+  //  // squaring each element of the lists
+  //  val func = (x: Int) => x * x
+  //
+  //  val res1 = l1.map(func)
+  //  val res2 = l2.map(func)
+  //
+  //  // 2 Immutable collections
+  //  val numbers = (1 to 10).toList
+  //  println(numbers)
+  //   val x = numbers.filter(_ > 3)
+  //     .filter(_ < 8)
+  //     .map(_ * 5)
+  //
+  //  println(x)
 
-  //  1.a = User function
 
-  val l1 = List(1, 1, 2, 3, 5, 8)
-  val l2 = List(13, 21, 34)
+  def sayHello(inputFunction: () => Unit): Unit = inputFunction()
 
-  // squaring each element of the lists
-  val func = (x: Int) => x * x
+  def helloSaber(): Unit = println("Hello, Saber")
 
-  val res1 = l1.map(func)
-  val res2 = l2.map(func)
-
-  // 2 Immutable collections
-  val numbers = (1 to 10).toList
-  println(numbers)
-   val x = numbers.filter(_ > 3)
-     .filter(_ < 8)
-     .map(_ * 5)
-
-  println(x)
+  sayHello(helloSaber)
 }
